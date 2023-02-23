965ee748f679d5cbb1ba4d0e3616eb2ef6afb0a5
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-02-23 05:26:10 +0000

    chore: created stream buffer funcion to create the string.

--- data_structures/3_doubly_linked_list.c
+++ data_structures/3_doubly_linked_list.c
@@ -2,8 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#define MAXBUFFER 10000
+
+struct node{
+  int data;
+  struct node *next;
+  struct node* prev;
+}*Head, *tmp, *tmp1;
+
+//function proto to create a stream of characters to print 
+//out the stat of thelist.
+char* create_list_buffer();
+char list_buffer[MAXBUFFER];
 
 int main(){
-  printf("test transmission!");
+  printf(" %s ", create_list_buffer());
   return 0;
+}; // end of main func.
+
+char* create_list_buffer(){
+  if(Head == NULL) return "[Empty list!]";
+  strcpy(list_buffer,""); //empty the buffer;
+  tmp = Head;
+  bool first_time =true;
+  while(tmp != NULL){
+    char data_str[15];
+    sprintf(data_str, "%d", tmp->data ); // conveting the int to a string.
+    if(first_time){
+      strcat(list_buffer, data_str); // concat only the value without the arrows on first loop.
+      first_time = false;
+    }else{
+      strcat(list_buffer, " <--> "); // add the chaining effect.
+      strcat(list_buffer, data_str); // add the data to the chain.
+      
+    };
+  }
+  return list_buffer;
 };
