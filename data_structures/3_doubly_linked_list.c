e1f6fc132b6deae2f29bfeccead6e17e217c7102
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-02-23 11:44:14 +0000

    chore: Added the insert_first_element function to the doubly_linked_list code.

--- data_structures/3_doubly_linked_list.c
+++ data_structures/3_doubly_linked_list.c
@@ -1,41 +1,117 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #define MAXBUFFER 10000
 
-struct node{
+struct node {
   int data;
   struct node *next;
-  struct node* prev;
-}*Head, *tmp, *tmp1;
+  struct node *prev;
+} * Head, *tmp, *tmp1;
 
-//function proto to create a stream of characters to print 
-//out the stat of thelist.
-char* create_list_buffer();
+// function proto to create a stream of characters to print
+// out the stat of thelist.
+char *create_list_buffer();
 char list_buffer[MAXBUFFER];
+char insert_first(int);
+char insert_last(int);
+char remove_first(int);
+char remove_last(int);
+char input_listener();
+void display_menu();
 
-int main(){
-  printf(" %s ", create_list_buffer());
+int main() {
+  char Exit_key = '6';
+  do {
+    display_menu(); // display the menu.
+  } while (input_listener() != Exit_key);
+
   return 0;
 }; // end of main func.
 
-char* create_list_buffer(){
-  if(Head == NULL) return "[Empty list!]";
-  strcpy(list_buffer,""); //empty the buffer;
+char *create_list_buffer() {
+  if (Head == NULL)
+    return "[Empty list!]";
+  strcpy(list_buffer, ""); // empty the buffer;
   tmp = Head;
-  bool first_time =true;
-  while(tmp != NULL){
+  bool first_time = true;
+  while (tmp != NULL) {
     char data_str[15];
-    sprintf(data_str, "%d", tmp->data ); // conveting the int to a string.
-    if(first_time){
-      strcat(list_buffer, data_str); // concat only the value without the arrows on first loop.
+    sprintf(data_str, "%d", tmp->data); // conveting the int to a string.
+    if (first_time) {
+      strcat(
+          list_buffer,
+          data_str); // concat only the value without the arrows on first loop.
       first_time = false;
-    }else{
+    } else {
       strcat(list_buffer, " <--> "); // add the chaining effect.
       strcat(list_buffer, data_str); // add the data to the chain.
-      
     };
+    tmp = tmp->next;
   }
   return list_buffer;
+};
+
+char insert_first(int value) {
+  if (Head == NULL) {
+    // when there is no head.
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->next = NULL;
+    tmp->data = value;
+    //printf("the value from the list %d", tmp->data);
+    tmp->prev = NULL;
+    Head = tmp;
+    tmp = NULL;
+    //printf("Heads data %d", Head->data);
+  } else {
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->next = Head;
+    tmp->data = value;
+    Head = tmp;
+    tmp = NULL;
+  };
+};
+char insert_last(int);
+char remove_first(int);
+char remove_last(int);
+
+char input_listener() {
+  char key;
+  scanf("%c", &key);
+  getchar();
+  //printf("the input is %c", key);
+  int value; // for the value to be inserted.
+  switch (key) {
+  case '1':
+    printf("\nEnter value to be inserted\n -> ");
+    scanf("%d", &value); // collecting the value.
+    getchar();
+    return insert_first(value);
+  case '2':
+  case '3':
+  case '4':
+  case '5':
+  case '6':
+    printf("\nFunction not implemented !");
+    return key;
+    break;
+  default:
+    break;
+  };
+  return key;
+}
+void display_menu() {
+  system("clear");
+  printf("*****************Doubly_linked_list*****************"
+         "\n\n");
+  printf("Items ::  %s", create_list_buffer());
+  printf("\n\n"
+         "\t1. Insert element to the begining of the list."
+         "\n\t2. Insert element to the end of the list."
+         "\n\t3. Remove element at the begining of the list."
+         "\n\t4. Remove element at the end of the list."
+         "\n\t5. Reset the element."
+         "\n\t6. Exit."
+         "\n\n -> ");
 };
